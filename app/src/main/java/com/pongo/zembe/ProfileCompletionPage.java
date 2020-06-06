f1b298f7cd3a029c664013a641d1704f2dc88a36
pongo1 <mrfimpong@gmail.com>
2020-06-06 07:40:37 +0000

    Profile completion page auto fill on start setup. Next >> getting to reading location coordinates

--- app/src/main/java/com/pongo/zembe/ProfileCompletionPage.java
+++ app/src/main/java/com/pongo/zembe/ProfileCompletionPage.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,7 +36,10 @@ public class ProfileCompletionPage extends AppCompatActivity {
   FirebaseFirestore db = FirebaseFirestore.getInstance();
   CollectionReference userDB = db.collection(Konstants.USER_COLLECTION);
   FirebaseAuth mAuth = FirebaseAuth.getInstance();
+  FirebaseUser user;
   ProgressBar spinner;
+  QuerySnapshot foundUserDocs;
+  LinearLayout elementsDiv;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +49,52 @@ public class ProfileCompletionPage extends AppCompatActivity {
     usernameBox = findViewById(R.id.prof_username);
     whatsappNumberBox = findViewById(R.id.prof_whatsapp_number);
     rootLayout = findViewById(R.id.coordinator_layout);
+    elementsDiv = findViewById(R.id.prof_mother_div);
+    elementsDiv.setVisibility(View.INVISIBLE);
     spinner = findViewById(R.id.prof_spinner);
+    user = mAuth.getCurrentUser();
+    if (user == null) {
+      goToLogin();
+    } else {
+      spinner.setVisibility(View.VISIBLE);
+      retrieveUserOldInfo(user);
+
+    }
+
+  }
+
+  private void goToLogin() {
+    Intent login = new Intent(this, Login.class);
+    startActivity(login);
+    finish();
+  }
+
+
+  public void retrieveUserOldInfo(FirebaseUser user) {
+    userDB.whereEqualTo("uniqueID", user.getUid()).get()
+      .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        @Override
+        public void onSuccess(QuerySnapshot documents) {
+          //this will actually only bring one item every time
+          //-------------------------------------------------
+          foundUserDocs = documents;
+          for (QueryDocumentSnapshot document : documents) {
+            if (document.exists()) {
+              GroundUser recoveredUser = document.toObject(GroundUser.class);
+              dobBox.setText(recoveredUser.getDOB());
+              usernameBox.setText(recoveredUser.getPreferredName());
+              whatsappNumberBox.setText(recoveredUser.getWhatsappNumber());
+              spinner.setVisibility(View.INVISIBLE);
+              elementsDiv.setVisibility(View.VISIBLE);
+            }
+          }
+        }
+      }).addOnFailureListener(new OnFailureListener() {
+      @Override
+      public void onFailure(@NonNull Exception e) {
+        Toast.makeText(ProfileCompletionPage.this, "For some reason, we could not update your profile. Please contact us if this error persists.", Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 
   public void saveUserInformation(View v) {
@@ -54,8 +103,6 @@ public class ProfileCompletionPage extends AppCompatActivity {
     name = usernameBox.getText().toString().trim();
     number = whatsappNumberBox.getText().toString().trim();
     HashMap<String, Object> dobValidation = RandomHelpersClass.validateDOB(dob);
-    FirebaseUser user = mAuth.getCurrentUser();
-
     if (name.isEmpty()) {
       usernameBox.setError("Sorry, you cant leave this empty");
       usernameBox.requestFocus();
@@ -89,59 +136,49 @@ public class ProfileCompletionPage extends AppCompatActivity {
     }
 
     spinner.setVisibility(View.VISIBLE);
-    userDB
-      .whereEqualTo("uniqueID", user.getUid())
-      .get()
-      .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-        @Override
-        public void onSuccess(QuerySnapshot documents) {
-          //this will actually only bring one item every time
-          //-------------------------------------------------
-          GroundUser recoveredUser;
-          for (QueryDocumentSnapshot document : documents) {
-            if (document.exists()) {
-              recoveredUser = document.toObject(GroundUser.class);
-              String documentID = document.getReference().getId();
-              recoveredUser.setWhatsappNumber(number);
-              recoveredUser.setDOB(dob);
-              recoveredUser.setPreferredName(name);
-              //reset old content with the newly updated version
-              //------------------------------------------------
-              userDB
-                .document(documentID).set(recoveredUser)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                  @Override
-                  public void onSuccess(Void aVoid) {
-                    spinner.setVisibility(View.INVISIBLE);
-                    ProfileCompletionDialog dialog = new ProfileCompletionDialog();
-                    dialog.show(getSupportFragmentManager(), "Profile Completion Congratulatory Message");
-                  }
-                }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                  Log.w("profileUpdateError:", e.getMessage());
-                  Snackbar.make(rootLayout, "For some reason, we could not update your profile. Please contact us if this error persists.", Snackbar.LENGTH_SHORT).show();
-                }
-              });
-            } else {
-              Snackbar.make(rootLayout, "This is weird, we could not find your profile", Snackbar.LENGTH_SHORT).show();
+    GroundUser recoveredUser;
+    for (QueryDocumentSnapshot document : foundUserDocs) {
+      if (document.exists()) {
+        recoveredUser = document.toObject(GroundUser.class);
+        String documentID = document.getReference().getId();
+        recoveredUser.setWhatsappNumber(number);
+        recoveredUser.setDOB(dob);
+        recoveredUser.setPreferredName(name);
+        //reset old content with the newly updated version
+        //------------------------------------------------
+        userDB
+          .document(documentID).set(recoveredUser)
+          .addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+              spinner.setVisibility(View.INVISIBLE);
+              ProfileCompletionDialog dialog = new ProfileCompletionDialog();
+              dialog.show(getSupportFragmentManager(), "Profile Update Congratulatory Message");
             }
+          }).addOnFailureListener(new OnFailureListener() {
+          @Override
+          public void onFailure(@NonNull Exception e) {
+            Log.w("profileUpdateError:", e.getMessage());
+            Snackbar.make(rootLayout, "For some reason, we could not update your profile. Please contact us if this error persists.", Snackbar.LENGTH_SHORT).show();
           }
-        }
-      })
-      .addOnFailureListener(new OnFailureListener() {
-        @Override
-        public void onFailure(@NonNull Exception e) {
-          Log.w("profileCompletionPage::", e.getMessage());
-          Snackbar.make(rootLayout, "Oops something happend, please try again later", Snackbar.LENGTH_SHORT).show();
-        }
-      });
+        });
+      } else {
+        Snackbar.make(rootLayout, "This is weird, we could not find your profile", Snackbar.LENGTH_SHORT).show();
+      }
+    }
+
   }
 
 
   public void goToMain(View v) {
     Intent main = new Intent(this, MainActivity.class);
     startActivity(main);
+
+  }
+
+  public void goToHomepage(View v) {
+    Intent home = new Intent(this, Home.class);
+    startActivity(home);
 
   }
 }
