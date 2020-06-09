c68d2aa7308bcdbab6475a19e502014c7b01816b
pongo1 <mrfimpong@gmail.com>
2020-06-09 09:31:09 +0000

    Zembe card page and notification tab design done. Next, messages design

--- app/src/main/java/com/pongo/zembe/NotificationFragment.java
+++ app/src/main/java/com/pongo/zembe/NotificationFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +15,10 @@ public class NotificationFragment extends Fragment {
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.notification_nav_fragment,container,false);
+//    View toolbarView = inflater.inflate(R.layout.app_default_toolbar,container,false);
+    View notificationFragmentView =  inflater.inflate(R.layout.notification_nav_fragment,container,false);
+//    TextView toolbarText = toolbarView.findViewById(R.id.toolbar_text);
+//    toolbarText.setText("Notifications");
+    return notificationFragmentView;
   }
 }
