a95573907abb368cc6e0d7186ee8a9a0bc8e103e
pongo1 <mrfimpong@gmail.com>
2020-06-17 23:55:07 +0000

    teach me how to create an errand page added

--- app/src/main/java/com/pongo/zembe/CreateNewErrand.java
+++ app/src/main/java/com/pongo/zembe/CreateNewErrand.java
@@ -3,15 +3,28 @@ package com.pongo.zembe;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateNewErrand extends AppCompatActivity {
   Toolbar toolbar;
+  Button teachMe;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_create_new_errand);
     toolbar = findViewById(R.id.app_default_toolbar);
     setSupportActionBar(toolbar);
+
+    teachMe = findViewById(R.id.teach_me);
+    teachMe.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent teachMePage = new Intent(CreateNewErrand.this, TeachMeHowToCreateAnErrand.class);
+        startActivity(teachMePage);
+      }
+    });
   }
 }
