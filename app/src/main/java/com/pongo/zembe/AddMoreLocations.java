05300c39168fbda7b40a3b60574e3ba29cfc03ce
pongo1 <mrfimpong@gmail.com>
2020-07-01 21:36:47 +0000

    location item onclick listener + deletion confirmation

--- app/src/main/java/com/pongo/zembe/AddMoreLocations.java
+++ app/src/main/java/com/pongo/zembe/AddMoreLocations.java
@@ -4,20 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class AddMoreLocations extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class AddMoreLocations extends AppCompatActivity implements LocationItemClicked {
 
   RecyclerView recyclerView;
+  LocationRecyclerAdapter adapter;
+  ArrayList<GallamseyLocationComponent> arr  = new ArrayList<>();
+  MagicBoxes magicBoxes;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    populate();
     setContentView(R.layout.activity_add_more_locations);
+    magicBoxes = new MagicBoxes(this);
     recyclerView = findViewById(R.id.location_list_recycler);
-    LocationRecyclerAdapter adapter = new LocationRecyclerAdapter(this,null);
+    adapter = new LocationRecyclerAdapter(this,arr, this);
     LinearLayoutManager manager = new LinearLayoutManager(this);
     recyclerView.setLayoutManager(manager);
     recyclerView.setAdapter(adapter);
+
+  }
+
+  public void populate(){
+
+      for(int i =0; i< 5; i++){
+        arr.add(new GallamseyLocationComponent("Location "+i,"32343.43454","6745673456.1"));
+      }
+
+  }
+
+  @Override
+  public void locationItemCallback(int position) {
+    GallamseyLocationComponent location = arr.get(position);
+    final int itemPosition = position;
+    magicBoxes.constructASimpleDialog("Confirm Location Deletion", "Are you sure you want to delete " + location.getLocationName(), new MagicBoxCallables() {
+      @Override
+      public void negativeBtnCallable() {
+        // do nothing
+      }
+
+      @Override
+      public void positiveBtnCallable() {
+        arr.remove(itemPosition);
+        adapter.notifyItemRemoved(itemPosition);
+      }
+    }).show();
 
   }
 }
--- app/src/main/java/com/pongo/zembe/LocationRecyclerAdapter.java
+++ app/src/main/java/com/pongo/zembe/LocationRecyclerAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,32 +14,48 @@ import java.util.ArrayList;
 public class LocationRecyclerAdapter extends RecyclerView.Adapter<LocationRecyclerAdapter.LocationRecyclerViewHolder> {
   Context context;
   ArrayList<GallamseyLocationComponent> locations;
+  LocationItemClicked itemClickedCallback ;
 
-  public LocationRecyclerAdapter(Context context, ArrayList<GallamseyLocationComponent> locations) {
+  public LocationRecyclerAdapter(Context context, ArrayList<GallamseyLocationComponent> locations, LocationItemClicked itemClickedCallback) {
     this.context = context;
     this.locations = locations;
+    this.itemClickedCallback = itemClickedCallback;
   }
 
   @NonNull
   @Override
   public LocationRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
     View v = LayoutInflater.from(context).inflate(R.layout.recyclable_location_item, parent, false);
-    return new LocationRecyclerViewHolder(v);
+    return new LocationRecyclerViewHolder(v, itemClickedCallback);
   }
 
   @Override
   public void onBindViewHolder(@NonNull LocationRecyclerViewHolder holder, int position) {
-
+    holder.locationName.setText(locations.get(position).getLocationName());
   }
 
   @Override
   public int getItemCount() {
-    return 3;
+    return locations.size();
   }
 
   public class LocationRecyclerViewHolder extends RecyclerView.ViewHolder {
-    public LocationRecyclerViewHolder(@NonNull View itemView) {
+    TextView locationName;
+    LocationItemClicked locationItemClicked;
+    public LocationRecyclerViewHolder(@NonNull View itemView, final LocationItemClicked locationItemClicked) {
       super(itemView);
+      this.locationItemClicked = locationItemClicked;
+      this.locationName = itemView.findViewById(R.id.location_list_item_name);
+
+      locationName.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          locationItemClicked.locationItemCallback(getAdapterPosition());
+        }
+      });
     }
   }
+}
+interface LocationItemClicked{
+  void locationItemCallback(int position);
 }
