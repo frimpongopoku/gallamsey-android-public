86a2b0df9feffe1caf68d95602950522f8b66515
Pongo1 <pongofrimi@gmail.com>
2020-07-04 18:38:24 +0000

    tab page switch on new errand creation page, left with location div, and image ON|OFF Fxnality

--- app/src/main/java/com/pongo/zembe/NewErrandCreationPage.java
+++ app/src/main/java/com/pongo/zembe/NewErrandCreationPage.java
@@ -1,32 +1,62 @@
 package com.pongo.zembe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
-public class NewErrandCreationPage extends AppCompatActivity implements OnDetailItemsClick {
+public class NewErrandCreationPage extends AppCompatActivity implements OnDetailItemsClick, View.OnClickListener {
 
-  ArrayList<String> dummyDetails = new ArrayList<>();
+  ArrayList<String> detailsList = new ArrayList<>();
   RecyclerView recyclerView;
-  ImageView  addDetailsBtn;
+  ImageView addDetailsBtn, descriptionTabBtn, estimateTabBtn, allowanceTabBtn, locationTabBtn, detailsTabBtn, addPictureTabBtn;
+  LinearLayout detailsTab, descriptionTab, estimationTab, allowanceTab, locationTab, pictureTab;
   EditText detailsBox;
   DetailsListAdapter recyclerAdapter;
+  Handler handler;
+  String currentTabKey = Konstants.DESC_TAB;
+
+  int DEFAULT_STATE_VALUE = 40, STATE_CHANGED_VALUE = 60;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_new_errand_creation_page);
+//  -----------------------------------------------------------
+    descriptionTabBtn = findViewById(R.id.tab_for_description_btn);
+    estimateTabBtn = findViewById(R.id.tab_for_estimated_cost_btn);
+    allowanceTabBtn = findViewById(R.id.tab_for_allowance_btn);
+    locationTabBtn = findViewById(R.id.tab_for_location_btn);
+    detailsTabBtn = findViewById(R.id.tab_for_details_btn);
+    addPictureTabBtn = findViewById(R.id.tab_for_image_btn);
+    detailsTab = findViewById(R.id.tab_for_details);
+    descriptionTab = findViewById(R.id.tab_for_description);
+    estimationTab = findViewById(R.id.tab_for_estimated_cost);
+    allowanceTab = findViewById(R.id.tab_for_allowance);
+    pictureTab = findViewById(R.id.tab_for_image);
+//  ..........................................................
+    descriptionTabBtn.setOnClickListener(onTabClick(Konstants.DESC_TAB, descriptionTabBtn, descriptionTab));
+    detailsTabBtn.setOnClickListener(onTabClick(Konstants.DETAILS_TAB, detailsTabBtn, detailsTab));
+    estimateTabBtn.setOnClickListener(onTabClick(Konstants.ESTIMATION_TAB, estimateTabBtn, estimationTab));
+    allowanceTabBtn.setOnClickListener(onTabClick(Konstants.ALLOWANCE_TAB, allowanceTabBtn, allowanceTab));
+
+//  ----------------------------------------------------------
     recyclerView = findViewById(R.id.recyclerview_in_details_tab);
     addDetailsBtn = findViewById(R.id.add_btn_in_details_tab);
     detailsBox = findViewById(R.id.edittext_in_details_tab);
-    recyclerAdapter = new DetailsListAdapter(this,dummyDetails,this);
+    recyclerAdapter = new DetailsListAdapter(this, detailsList, this);
     LinearLayoutManager manager = new LinearLayoutManager(this);
     recyclerView.setLayoutManager(manager);
     recyclerView.setAdapter(recyclerAdapter);
@@ -35,20 +65,127 @@ public class NewErrandCreationPage extends AppCompatActivity implements OnDetail
   }
 
 
+  public void showTabState() {
+    descriptionTabBtn.setImageResource(R.drawable.ic_description_green);
+    descriptionTabBtn.getLayoutParams().height = 55;
+    descriptionTabBtn.getLayoutParams().width = 55;
+  }
+
+
+  private View.OnClickListener onTabClick(final String whichPage, final ImageView newPageBtn, final LinearLayout newPage) {
+    return new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        takeMeToThisTab(whichPage, newPageBtn, newPage);
+      }
+    };
+  }
+
+  private void takeMeToThisTab(String whichPage, ImageView newPageBtn, LinearLayout newPage) {
+    if (currentTabKey.equals(whichPage)) {
+      return;
+    }
+    ImageView oldPageBtn = bringMeTabButton(currentTabKey);
+    LinearLayout oldPage = bringMeTabPage(currentTabKey);
+    changeTabBtnState(oldPageBtn, Konstants.INACTIVE);
+    changeTabBtnState(newPageBtn, Konstants.ACTIVE);
+    oldPage.setVisibility(View.GONE);
+    newPage.setVisibility(View.VISIBLE);
+    currentTabKey = whichPage;
+
+  }
+
+  private void changeTabBtnState(ImageView btn, String how) {
+    switch (how) {
+      case Konstants.INACTIVE: {
+        btn.getLayoutParams().width = DEFAULT_STATE_VALUE;
+        btn.getLayoutParams().height = DEFAULT_STATE_VALUE;
+        break;
+      }
+      case Konstants.ACTIVE: {
+        btn.getLayoutParams().width = STATE_CHANGED_VALUE;
+        btn.getLayoutParams().height = STATE_CHANGED_VALUE;
+        break;
+      }
+    }
+    btn.requestLayout();
+  }
+
+  private ImageView bringMeTabButton(String whichTab) {
+    switch (whichTab) {
+      case Konstants.IMAGE_TAB: {
+        return addPictureTabBtn;
+      }
+      case Konstants.DESC_TAB: {
+        return descriptionTabBtn;
+      }
+      case Konstants.LOCATION_TAB: {
+        return locationTabBtn;
+      }
+      case Konstants.DETAILS_TAB: {
+        return detailsTabBtn;
+      }
+      case Konstants.ESTIMATION_TAB: {
+        return estimateTabBtn;
+      }
+      case Konstants.ALLOWANCE_TAB: {
+        return allowanceTabBtn;
+      }
+    }
+    return null;
+  }
+
+  private LinearLayout bringMeTabPage(String whichTab) {
+
+    switch (whichTab) {
+      case Konstants.IMAGE_TAB: {
+        return pictureTab;
+      }
+      case Konstants.DESC_TAB: {
+        return descriptionTab;
+      }
+      case Konstants.LOCATION_TAB: {
+        return locationTab;
+      }
+      case Konstants.DETAILS_TAB: {
+        return detailsTab;
+      }
+      case Konstants.ALLOWANCE_TAB: {
+        return allowanceTab;
+      }
+      case Konstants.ESTIMATION_TAB: {
+        return estimationTab;
+      }
+    }
+    return null;
+  }
+
+  private void handleRepeatition() {
+    new Handler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+
+        Log.d("printingPress", "print me every second... do it....");
+        handleRepeatition();
+      }
+    }, 1000);
+  }
+
   private View.OnClickListener addToDetailsList = new View.OnClickListener() {
     @Override
     public void onClick(View view) {
-      if(validateDetails()){
-        dummyDetails.add(detailsBox.getText().toString().trim());
+      if (validateDetails()) {
+        detailsList.add(detailsBox.getText().toString().trim());
         recyclerAdapter.notifyDataSetChanged();
         detailsBox.setText("");
       }
     }
   };
-  public Boolean validateDetails(){
-    Boolean good  =true;
+
+  public Boolean validateDetails() {
+    Boolean good = true;
     String det = detailsBox.getText().toString().trim();
-    if(det.isEmpty()){
+    if (det.isEmpty()) {
       detailsBox.setError("Add a specific instruction before you add");
       detailsBox.requestFocus();
       good = false;
@@ -58,6 +195,18 @@ public class NewErrandCreationPage extends AppCompatActivity implements OnDetail
 
   @Override
   public void onDetailItemClick(int pos) {
+    detailsList.remove(pos);
+    recyclerAdapter.notifyItemRemoved(pos);
+  }
 
+  @Override
+  public void onClick(View view) {
+    switch (view.getId()) {
+      case R.id.tab_for_allowance_btn: {
+
+      }
+
+
+    }
   }
 }
