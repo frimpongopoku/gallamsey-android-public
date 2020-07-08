c14c688a8a90e919a12b0215785bf4e85118ea5e
Pongo1 <pongofrimi@gmail.com>
2020-07-08 10:10:07 +0000

    Some important reshuffling :stuck_out_tongue:

---
+++ app/src/main/java/com/pongo/zembe/OfficialSettingsPage.java
@@ -0,0 +1,14 @@
+package com.pongo.zembe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OfficialSettingsPage extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_official_settings_page);
+  }
+}
