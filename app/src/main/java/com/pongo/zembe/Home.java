0e8e8790b5e516040c4415626e80ca8d60111ef6
Pongo1 <pongofrimi@gmail.com>
2020-07-10 23:36:23 +0000

    Cropper and image compression now impl.. on create-errand page :ok_hand: :sparkles:

--- app/src/main/java/com/pongo/zembe/Home.java
+++ app/src/main/java/com/pongo/zembe/Home.java
@@ -193,6 +193,7 @@ public class Home extends AppCompatActivity {
 
   public void goToProfileViewPage(View v) {
     Intent profile = new Intent(this, ViewProfilePage.class);
+    profile.putExtra("authUser",authenticatedUser);
     startActivity(profile);
     this.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
   }
