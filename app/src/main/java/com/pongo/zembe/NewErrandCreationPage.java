3cfdfacf5198ed11787d2b7c6c12a5d9be95c65a
Pongo1 <pongofrimi@gmail.com>
2020-07-13 02:03:41 +0000

    Errand Creation XML split into diff layouts :ok_hand: :construction:

--- app/src/main/java/com/pongo/zembe/NewErrandCreationPage.java
+++ app/src/main/java/com/pongo/zembe/NewErrandCreationPage.java
@@ -16,6 +16,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
