fd63293b46cff30f3bda4aeef7ee9f86a2957d86
Pongo1 <pongofrimi@gmail.com>
2020-07-16 01:48:44 +0000

    Simmer effect (skeleton view) :ok_hand: news now points to errand adapter :muscle: :sparkles:

--- app/src/main/java/com/pongo/zembe/HomeFragment.java
+++ app/src/main/java/com/pongo/zembe/HomeFragment.java
@@ -2,17 +2,29 @@ package com.pongo.zembe;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.facebook.shimmer.Shimmer;
+import com.facebook.shimmer.ShimmerFrameLayout;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,8 +36,15 @@ public class HomeFragment extends Fragment {
 
   HashMap<String, ArrayList<String>> items;
   ArrayList<String> test = new ArrayList<>();
+  FirebaseFirestore store = FirebaseFirestore.getInstance();
+  CollectionReference errandsDB = store.collection(Konstants.ERRAND_COLLECTION);
 
-  public HomeFragment(  HashMap<String, ArrayList<String>> items) {
+  @Override
+  public void setAllowEnterTransitionOverlap(boolean allow) {
+
+  }
+
+  public HomeFragment(HashMap<String, ArrayList<String>> items) {
     this.items = items;
 
   }
@@ -34,11 +53,54 @@ public class HomeFragment extends Fragment {
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.home_nav_fragment, container, false);
-    HomeNewsMultiAdapter adapter = new HomeNewsMultiAdapter(getContext(),items.get("descs"),items.get("profits"), items.get("costs"), items.get("dates"));
-    RecyclerView recyclerView = view.findViewById(R.id.home_news_recycler);
+    final ShimmerFrameLayout skeleton = view.findViewById(R.id.news_skeleton_view);
+    skeleton.startShimmer();
+    HomeNewsMultiAdapter adapter = new HomeNewsMultiAdapter(getContext(), new ArrayList<GenericErrandClass>());
+    final RecyclerView recyclerView = view.findViewById(R.id.home_news_recycler);
     recyclerView.setAdapter(adapter);
     RecyclerView.LayoutManager manager = new LinearLayoutManager(getContext());
     recyclerView.setLayoutManager(manager);
+    getNewsFromFirebase(new NewsCollectionCallback() {
+      @Override
+      public void getErrands(ArrayList<GenericErrandClass> errands) {
+        HomeNewsMultiAdapter newsAdapter = new HomeNewsMultiAdapter(getContext(), errands);
+        recyclerView.setAdapter(newsAdapter);
+        skeleton.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
+      }
+    });
+//    new Handler().postDelayed(new Runnable() {
+//      @Override
+//      public void run() {
+//        skeleton.stopShimmer();
+//        skeleton.setVisibility(View.GONE);
+//        recyclerView.setVisibility(View.VISIBLE);
+//      }
+//    }, 2000);
     return view;
+  }
+
+  private void getNewsFromFirebase(final NewsCollectionCallback newsCallback) {
+    errandsDB.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+      @Override
+      public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+        ArrayList<GenericErrandClass> list = new ArrayList<>();
+        for (DocumentSnapshot document : queryDocumentSnapshots) {
+          list.add(document.toObject(GenericErrandClass.class));
+        }
+
+        newsCallback.getErrands(list);
+      }
+    }).addOnFailureListener(new OnFailureListener() {
+      @Override
+      public void onFailure(@NonNull Exception e) {
+        e.printStackTrace();
+        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
+
+  private interface NewsCollectionCallback {
+    void getErrands(ArrayList<GenericErrandClass> errands);
   }
 }
--- app/src/main/java/com/pongo/zembe/HomeNewsMultiAdapter.java
+++ app/src/main/java/com/pongo/zembe/HomeNewsMultiAdapter.java
@@ -6,13 +6,17 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -21,18 +25,14 @@ public class HomeNewsMultiAdapter extends RecyclerView.Adapter {
   int IMAGE_CODE = 6;
   int TEXT_CODE = 2;
   String TAG = "Adapter has started...";
-  ArrayList<String> Descriptions = new ArrayList<>();
-  ArrayList<String> Profits = new ArrayList<>();
-  ArrayList<String> Costs = new ArrayList<>();
-  ArrayList<String> Dates = new ArrayList<>();
-  Context Context;
 
-  public HomeNewsMultiAdapter(Context context, ArrayList<String> descriptions, ArrayList<String> profits, ArrayList<String> costs, ArrayList<String> dates) {
-    Descriptions = descriptions;
-    Profits = profits;
-    Costs = costs;
-    Dates = dates;
-    this.Context = context;
+  ArrayList<GenericErrandClass> news = new ArrayList<>();
+  Context context;
+
+  public HomeNewsMultiAdapter(Context context, ArrayList<GenericErrandClass> news) {
+
+    this.context = context;
+    this.news = news;
 
   }
 
@@ -54,16 +54,18 @@ public class HomeNewsMultiAdapter extends RecyclerView.Adapter {
 
   @Override
   public int getItemViewType(int position) {
-    if (position % 2 == 0) {
-      return TEXT_CODE;
+    GenericErrandClass newsItem = news.get(position);
+    if (newsItem.getErrandType().equals(Konstants.IMAGE_ERRAND)) {
+      return IMAGE_CODE;
     }
-    return IMAGE_CODE;
+    return TEXT_CODE;
   }
 
   @Override
   public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
-    if (position % 2 == 0) {
+    GenericErrandClass item = news.get(position);
+    if (item.getErrandType().equals(Konstants.TEXT_ERRAND)) {
       setTextContent(holder, position);
     } else {
       setImageContent(holder, position);
@@ -74,32 +76,35 @@ public class HomeNewsMultiAdapter extends RecyclerView.Adapter {
 
   public void setTextContent(@NonNull RecyclerView.ViewHolder _holder, int pos) {
     TextViewHolder holder = (TextViewHolder) _holder;
-    holder.description.setText(Descriptions.get(pos));
-    holder.cost.setText(Costs.get(pos));
-    holder.profit.setText(Profits.get(pos));
-    holder.date.setText(Dates.get(pos));
+    GenericErrandClass newsItem = news.get(pos);
+    holder.container.setAnimation(AnimationUtils.loadAnimation(context, R.anim.fade_in_recycler));
+    holder.description.setText(newsItem.getDescription());
+    holder.cost.setText(String.valueOf(newsItem.getCost()));
+    holder.profit.setText(String.valueOf(newsItem.getAllowance()));
+    holder.date.setText(newsItem.getCreatedAt());
   }
 
   public void setImageContent(@NonNull RecyclerView.ViewHolder _holder, int pos) {
     ImageViewHolder holder = (ImageViewHolder) _holder;
-    holder.title.setText(Descriptions.get(pos));
-    holder.cost.setText(Costs.get(pos));
-    holder.profit.setText(Profits.get(pos));
-    holder.date.setText(Dates.get(pos));
-//    holder.image.setImageResource(R.drawable.shoe50);
-
+    GenericErrandClass newsItem = news.get(pos);
+    holder.container.setAnimation(AnimationUtils.loadAnimation(context, R.anim.fade_in_recycler));
+    holder.title.setText(newsItem.getDescription());
+    holder.cost.setText(String.valueOf(newsItem.getCost()));
+    holder.profit.setText(String.valueOf(newsItem.getAllowance()));
+    holder.date.setText(newsItem.getCreatedAt());
+    Picasso.get().load(newsItem.getImages().get(0)).into(holder.image);
   }
 
   @Override
   public int getItemCount() {
-    return this.Descriptions.size();
+    return this.news.size();
   }
 
 
-
   //........ TextCard
-  class TextViewHolder extends RecyclerView.ViewHolder  {
+  class TextViewHolder extends RecyclerView.ViewHolder {
     TextView description, cost, profit, date, has_specifics;
+    LinearLayout container;
 
     public TextViewHolder(@NonNull View itemView) {
       super(itemView);
@@ -107,10 +112,11 @@ public class HomeNewsMultiAdapter extends RecyclerView.Adapter {
       this.cost = itemView.findViewById(R.id.text_errand_card_cost);
       this.profit = itemView.findViewById(R.id.text_errand_card_allowance);
       this.date = itemView.findViewById(R.id.text_errand_card_date);
+      this.container = itemView.findViewById(R.id.container);
       itemView.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-          Intent errandViewPage = new Intent(view.getContext(),ErrandViewActivity.class);
+          Intent errandViewPage = new Intent(view.getContext(), ErrandViewActivity.class);
           view.getContext().startActivity(errandViewPage);
         }
       });
@@ -124,6 +130,7 @@ public class HomeNewsMultiAdapter extends RecyclerView.Adapter {
   class ImageViewHolder extends RecyclerView.ViewHolder {
     TextView title, cost, profit, date, has_specifics;
     ImageView image;
+    LinearLayout container;
 
     public ImageViewHolder(@NonNull View itemView) {
       super(itemView);
@@ -132,10 +139,11 @@ public class HomeNewsMultiAdapter extends RecyclerView.Adapter {
       this.profit = itemView.findViewById(R.id.img_errand_card_allowance);
       this.date = itemView.findViewById(R.id.img_errand_card_date);
       this.image = itemView.findViewById(R.id.img_errand_card_image);
+      this.container = itemView.findViewById(R.id.container);
       itemView.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-          Intent imageErrandPage = new Intent(view.getContext(),ImageErrandView.class);
+          Intent imageErrandPage = new Intent(view.getContext(), ImageErrandView.class);
           view.getContext().startActivity(imageErrandPage);
         }
       });
