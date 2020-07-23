84438c133574b12a6e3af2a3ca6d39db2c057323
Pongo1 <pongofrimi@gmail.com>
2020-07-23 00:21:06 +0000

    Shimmer effect for search added :bus:

--- app/src/main/java/com/pongo/zembe/SearchAnythingActivity.java
+++ app/src/main/java/com/pongo/zembe/SearchAnythingActivity.java
@@ -3,12 +3,28 @@ package com.pongo.zembe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class SearchAnythingActivity extends AppCompatActivity {
+
+  EditText searchBox;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_search_anything);
+    searchBox = findViewById(R.id.search_box);
+    searchBox.requestFocus();
+    showKeyBoardOnLoad();
+
   }
+
+
+  private void showKeyBoardOnLoad() {
+    getWindow().setSoftInputMode (WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+  }
+
+
 }
