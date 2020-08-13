6128e9a6402621d70619d173d2aa09a3d7aba7bc
Pongo1 <pongofrimi@gmail.com>
2020-08-13 15:57:17 +0000

    Cleaning up messaging page :bowtie: :speech_balloon:

--- app/src/main/java/com/pongo/zembe/ChattingPage.java
+++ app/src/main/java/com/pongo/zembe/ChattingPage.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -12,14 +14,16 @@ public class ChattingPage extends AppCompatActivity {
 
   LinearLayout layout;
   RelativeLayout relativeLayout;
+  ImageView receipientImg;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_chatting_page);
+    receipientImg = findViewById(R.id.profile_icon);
+    receipientImg.setVisibility(View.VISIBLE);
     RecyclerView recyclerView = findViewById(R.id.chatting_recycler);
     ChattingAdapter adapter = new ChattingAdapter();
     LinearLayoutManager manager = new LinearLayoutManager(this);
-//    manager.addView(relativeLayout);
     recyclerView.setLayoutManager(manager);
     recyclerView.setAdapter(adapter);
   }
